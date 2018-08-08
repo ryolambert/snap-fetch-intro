c887269e9935a1ca53f972c431a2d2cd7c30630c
James R. Lambert <ryolambert@gmail.com>
2018-08-08 13:49:14 -0600

    yup

---
+++ script.js
@@ -0,0 +1,16 @@
+fetch('https://jsonplaceholder.typicode.com/posts')
+	.then(res => res.json())
+	.then(data => console.log(data))
+	.then((data) => {
+		let output = '<h3> Posts </h3>';
+		data.forEach(function (user){
+			output += `
+			<ul>
+				<li>ID: ${user.id}</li>
+				<li>Title: ${user.title}</li>
+				<li>Body: ${user.body}</li>
+			</ul>
+			`;
+			document.getElementById('fetchOutput').innerHTML = output
+		});
+	});
